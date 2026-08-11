47d24d604919c8d6875327858250a687f99e6d85
toommyliu <86510873+toommyliu@users.noreply.github.com>
2026-08-10 20:36:24 -0700

    feat(docs): add enhancements guide

--- docs/components.ts
+++ docs/components.ts
@@ -1,10 +1,15 @@
 import { defineComponents } from "blume";
 
+import EnhancementLabels from "./components/EnhancementLabels.astro";
+
 export default defineComponents({
   layout: {
     Header: "./components/Header.astro",
     PageFooter: "./components/DocsController.astro",
     Search: "./components/Search.astro",
     TableOfContents: "./components/TableOfContents.astro",
+  },
+  mdx: {
+    EnhancementLabels,
   },
 });
--- docs/src/content/docs/guides/scripting/meta.ts
+++ docs/src/content/docs/guides/scripting/meta.ts
@@ -1,6 +1,6 @@
 import { defineMeta } from "blume";
 
 export default defineMeta({
-  pages: ["script-format", "editor-setup"],
+  pages: ["script-format", "enhancements", "editor-setup"],
   title: "Scripting",
 });
