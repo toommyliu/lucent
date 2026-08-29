2c7b154c1695fc6744cd3f2de76b5736ee4bd5ce
Tommy Liu <86510873+toommyliu@users.noreply.github.com>
2026-08-28 23:48:43 -0700

    docs: add installation guide (#142)

--- docs/src/content/docs/guides/meta.ts
+++ docs/src/content/docs/guides/meta.ts
@@ -1,6 +1,6 @@
 import { defineMeta } from "blume";
 
 export default defineMeta({
-  pages: ["index", "combat-profiles", "scripting"],
+  pages: ["index", "installation", "combat-profiles", "scripting"],
   title: "Guides",
 });
